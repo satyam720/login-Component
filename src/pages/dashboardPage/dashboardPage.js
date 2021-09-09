2c3537e00b807a0b7feaaf5dd1206e196838fd12
satyam1906 <psatyam1618@gmail.com>
2021-09-09 14:16:46 +0530

    styled and changed dashboard

--- src/pages/dashboardPage/dashboardPage.js
+++ src/pages/dashboardPage/dashboardPage.js
@@ -14,17 +14,26 @@ const HandleClick= () => {
 }
     
     return(
-        <div className="dashboardPage">
-         <Card title="User DashBoard"  style={{ width: 500 }}>
-         <h1>Welcome {displayName}</h1>
+       <div className="container">
+        <div className="container dashboardPage">
+         <Card  title="User DashBoard"  style={{ width: 500 }}
+         headStyle={{backgroundColor: '#8236CB',color: 'white', border: 0 }}
+bodyStyle={{backgroundColor: '#610094',color: 'white', border: 0 }}>
+         <h1 style={{color: 'white'}}>Welcome {displayName}</h1>
          <p>You have signed in using {email} </p>
-         <Button size='large' className="btn btn-outline-dark edit " onClick={HandleClick}><span style={{fontWeight: 'bold'}}>Sign Out</span> </Button>
+         <Button size='large' className="btn edit" onClick={HandleClick}><span style={{fontWeight: 'bold'}}>Sign Out</span> </Button>
     </Card>
-            
-            
-            
-
-        </div>
+    </div>
+    <div className="info">
+        <h1>About This Login Component:</h1>
+        <p>The component allows Users To login using firebase google Authentication.
+        <br />
+        The data shown in The dashboard is taken from firebase auth snapshot.
+        <br />The project uses ReactJs, Firebase, react-router-dom, Antd Framework(Tooltip, Buttons), react-toastify for notifications, sass, bootstrap, css.</p>
+        
+    </div>
+    </div>
+      
     )};
 
 
